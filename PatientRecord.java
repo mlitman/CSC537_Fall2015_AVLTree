26c86deb99372929412afb398d3346fb19cffaf4
Mike Litman <mike@awesomefat.com>
2015-11-18 21:45:57 -0600

    initial commit

---
+++ PatientRecord.java
@@ -0,0 +1,26 @@
+
+public class PatientRecord 
+{
+	private String fname;
+	private String lname;
+	private int age;
+	
+	public PatientRecord(String fname, String lname, int age)
+	{
+		this.fname = fname;
+		this.lname = lname;
+		this.age = age;
+	}
+	
+	public void display()
+	{
+		System.out.println("Patient: " + this.fname + " " + this.lname + " age: " + this.age);
+	}
+
+	public int getAge() 
+	{
+		return age;
+	}
+	
+	
+}
